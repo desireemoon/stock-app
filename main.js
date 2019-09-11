1654159e43f76b7e64f5abe9ce71cb329ac3c341
Desiree deWysocki <desiree.dewysocki@gmail.com>
2019-09-11 10:36:44 -0400

    js variables

---
+++ main.js
@@ -0,0 +1,12 @@
+let body = document.body
+let companyLogo = document.querySelector("img")
+let name = document.querySelector("#name")
+let description = document.querySelector("#description")
+let stockToday = document.querySelector("#stock-today")
+let maxStock = document.querySelector("#max-stock")
+let minStock = document.querySelector("#min-stock")
+let newsArticles = document.querySelector("#news-articles")
+
+//grab api setup connection
+//grab data connect it to html 
+
